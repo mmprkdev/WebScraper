b37503268cf14bb44cab64a05d20cdfefef6e950
Matt Park <mmprkdev@gmail.com>
2017-11-19 10:57:02 -0600

    first commit

---
+++ WebScraper/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WebScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameRankingsDataView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gameRankingsDataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameRankingsDataView
+            // 
+            this.gameRankingsDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gameRankingsDataView.Location = new System.Drawing.Point(12, 12);
+            this.gameRankingsDataView.Name = "gameRankingsDataView";
+            this.gameRankingsDataView.Size = new System.Drawing.Size(285, 436);
+            this.gameRankingsDataView.TabIndex = 0;
+            this.gameRankingsDataView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gameRankingsDataView_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(321, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Collects the name and the score of all games shown at \r\nhttps://www.gamerankings." +
+    "com/browse.html";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(321, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 460);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gameRankingsDataView);
+            this.Name = "Form1";
+            this.Text = "Game Rankings Web Scraper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gameRankingsDataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gameRankingsDataView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WebScraper/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        HtmlWeb web = new HtmlWeb();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            InitTable();
+            int count = 0;
+            label2.Text = "Collecting data...";
+            label2.Visible = true;
+            label3.Text = "Number of elements: 0";
+            label3.Visible = true;
+
+            int pageNumber = 0;
+            var rankings = await GameRankingsFromPage(0);
+
+            while (rankings.Count > 0)
+            {
+                foreach (var ranking in rankings)
+                {
+                    table.Rows.Add(ranking.Name, ranking.Score);
+                    count++;
+                    label3.Text = "Number of rows: " + count.ToString();
+                }
+                rankings = await GameRankingsFromPage(++pageNumber);
+            }
+            label2.Text = "Finished";
+        }
+
+        // NOTE: To get the XPath for an element, right click on the element on the webpage, click on
+        // Inspect Element, right click on the element in the inpector window, go to copy and choose
+        // the XPath option.
+        // NOTE: make sure to remove tbody in tables. Replacing [n] with // will increase the number of nodes.
+        private async Task<List<NameAndScore>> GameRankingsFromPage(int pageNumber)
+        {
+            string url = "https://www.gamerankings.com/browse.html";
+            // Need to add "?page=n" to access other pages. 
+            if (pageNumber != 0)
+            {
+                url = "https://www.gamerankings.com/browse.html?page=" + pageNumber.ToString();
+            }
+
+            var doc = await Task.Factory.StartNew(() => web.Load(url));
+            var nameNodes = doc.DocumentNode.SelectNodes("/html/body/div/div[5]/div[2]/div[1]/div/div[2]/div[2]/table//tr//td//a");
+            var scoreNodes = doc.DocumentNode.SelectNodes("/html/body/div/div[5]/div[2]/div[1]/div/div[2]/div[2]/table//tr//td//span/b");
+
+            if (nameNodes == null || scoreNodes == null)
+            {
+                // nodes could not be found, return empty list
+                return new List<NameAndScore>();
+            }
+
+            var names = nameNodes.Select(node => node.InnerText);
+            var scores = scoreNodes.Select(score => score.InnerText);
+
+            return names.Zip(scores, (name, score) => new NameAndScore() { Name = name, Score = score}).ToList();
+        }
+
+        private void InitTable()
+        {
+            table = new DataTable("GameRankingsDataTable");
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Score", typeof(string));
+
+            gameRankingsDataView.DataSource = table;
+        }
+
+        private void gameRankingsDataView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        // xpath for steampowered top sellers
+        // /html/body/div[1]/div[7]/div[3]/div[1]/div[13]/div/div[1]/div[2]/div[2]/a[1]/div[3]/div[1]
+
+    }
+
+    // using this to set name and score values
+    public class NameAndScore
+    {
+        public string Name { get; set; }
+        public string Score { get; set; }
+    }
+}
